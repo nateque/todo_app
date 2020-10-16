2afd32871dfc203f7ea860563f88a9420aa98e38
Nateque Siddiqui <mdnateque@gmail.com>
2020-10-16 21:49:18 +0530

    Done some corrections

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,7 @@ from .forms import TaskForm
 
 # Create your views here.
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-created')
     form = TaskForm()
 
     if request.method == 'POST':
